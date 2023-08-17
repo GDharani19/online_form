972f822c01ae2291f6892a77f2af5b724907426e
GDharani19 <gottamdharani7626@gmail.com>
2023-08-17 22:11:10 +0530

    online form

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class feedbackmodel(models.Model):
+    Name=models.CharField(max_length=50)
+    age=models.PositiveIntegerField()
+    Email=models.EmailField()
+    mobile=models.PositiveBigIntegerField()
+    desc=models.TextField(max_length=500)
+
